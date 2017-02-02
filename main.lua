b4c8a051978739c034e50dc50cea2df39abbf608
sergienko_ev <eor@tsogu.ru>
2017-02-02 17:01:19 +0500

    Готово

--- main.lua
+++ main.lua
@@ -6,8 +6,7 @@
 
 -- Your code here
 -- Печатаем в консоли
-print( "Hello World!" )
-
+print( "Hello World!" ..5)
 
 local physics = require( "physics" )
 physics.start()
@@ -23,7 +22,7 @@ physics.addBody( ground, "static", { friction=0.5, bounce=0.3 } )
 cratesGroup=display.newGroup( )
 crates={}
 
-for a=1,3,1 do
+for a=1,10,1 do
 crates[a] = display.newImage("crate.png")	
 crates[a].x=30*a+display.contentCenterX
 crates[a].y=50*a
@@ -34,11 +33,12 @@ end
 --text
 local myTextObject = display.newText( "Hello Game!", display.contentCenterX, 0, "Arial", 60 )
 myTextObject:setFillColor( 1,0,0 )
+
 function screenTap()
-    local r = math.random( 0, 100 )
+    local r = math.random( 0, 255 )
     local g = math.random( 0, 100 )
     local b = math.random( 0, 100 )
-    myTextObject:setFillColor( r/100, g/100, b/100 )
+    myTextObject:setFillColor( r/255, g/100, b/100 )
 end
 
 display.currentStage:addEventListener( "tap", screenTap )
