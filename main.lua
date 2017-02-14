7e6e645425bbfd0e94873f5b15afb92649433b66
sergienko_ev <eor@tsogu.ru>
2017-02-14 11:55:23 +0500

    пузырьковая сортировка массива добавлена

--- main.lua
+++ main.lua
@@ -34,25 +34,58 @@ local function CirleSquare(radius)
 	return s
 end
 
-local r=10
+local r=50
 local s=CirleSquare(r)
 print("Площадь круга с радиусом "..r.." = "..s)
---------------------------------------------------------------------
 
+--------------------------------------------------------------------
 local myText=display.newText( "-", display.contentCenterX, 50, "Arial", 30 )
 myText:setFillColor( 1,0,0 )
 
+local tab={1,7,3,4,2}
+
+------------------------------------------ вывести на экран таблицу(массив)
+local function PrintTable(t)
+	for i, j in pairs(t) do
+		print("Элемент "..i.." равен: "..j)
+	end	
+end
+----------------------------------------- Сортировка массива (Пузырьковая)
+function bubble_sort(arr)
+	for i=table.getn(arr), 2, -1  do 
+		for j=2, i do				 
+				if arr[j] < arr[j-1] then
+				tmp = arr[j-1]
+				arr[j-1] = arr[j]
+				arr[j] = tmp
+			end
+		end
+	end
+	return arr
+end
+
+-----------------------------------------
 
 local function  handleButtonEvent( event )
 	local phase=event.phase
-	if "ended" then
+	if "ended"==event.phase then
 		s=CirleSquare(10)
 		myText.text=s.."см2"
+		PrintTable(tab)
+		sortedTab=bubble_sort(tab)
+		print( "Отсортированный массив" )
+		PrintTable(sortedTab)
+
 	end
 	-- body
 end
 
-local myButton=widget.newButton
+
+
+
+
+
+local myButton=widget.newButton(
 {
 	
 	top=200,
@@ -61,13 +94,10 @@ local myButton=widget.newButton
 	left=100,
 	defaultFile="button.png",
 	overFile="buttonOver.png",
-	label="Площадь круга радиусом 10см",
+	label="Площадь круга радиусом " ..r.." см",
 	onEvent=handleButtonEvent,
-
-}
-
-
-
+	labelColor = { default={ 1, 1, 1 }, over={ 0, 0, 0, 0.5 } }
+})
 
 
 
